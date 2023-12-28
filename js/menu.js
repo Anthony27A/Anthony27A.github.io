de5e91d82b6b1b5d9255a312005f41f4ad2ea4ec
Sali2003-alt <154335113+Sali2003-alt@users.noreply.github.com>
2023-12-28 13:45:57 -0500

    agregar linea

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+var menu=
+
+'<ul class="clase1">'+
+    '<li class="formulario"><a href="salome.html">Salomé</a></li>'+
+    '<li class="formulario"><a href="anthony.html">Anthony</a></li>'+
+    '<li class="formulario"><a href="jomara.html">Jomara</a></li>'+
+    '<li class="formulario"><a href="adriana.html">Adriana</a></li>'+
+    '</ul>';
+    document.getElementById('contenedor-menu').
+    innerHTML=menu;
