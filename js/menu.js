7984f61ddd5ab27abfff6d4eabfa5820982b0c7c
Anthony27A <154335234+Anthony27A@users.noreply.github.com>
2024-01-01 20:40:11 -0500

    agregar linea

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,9 @@ var menu=
     '<li class="formulario"><a href="salome.html">Salomé</a></li>'+
     '<li class="formulario"><a href="anthony.html">Anthony</a></li>'+
     '<li class="formulario"><a href="jomara.html">Jomara</a></li>'+
-    '<li class="formulario"><a href="index.html">Inicio</a></li>'+
     '<li class="formulario"><a href="adriana.html">Adriana</a></li>'+
+    '<li class="formulario"><a href="index.html">Inicio</a></li>'+
+    
     '</ul>';
     document.getElementById('contenedor-menu').
     innerHTML=menu;
