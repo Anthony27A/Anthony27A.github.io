cfcd6993150265a8f681d72f94276d16bdcb5094
Sali2003-alt <154335113+Sali2003-alt@users.noreply.github.com>
2024-01-07 18:58:43 -0500

    .

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 var menu=
 
 '<ul class="clase1">'+
-    '<li class="formulario"><a href="salome.html">Salomé</a></li>'+
     '<li class="formulario"><a href="anthony.html">Anthony</a></li>'+
+    '<li class="formulario"><a href="salome.html">Salomé</a></li>'+
     '<li class="formulario"><a href="jomara.html">Jomara</a></li>'+
     '<li class="formulario"><a href="adriana.html">Adriana</a></li>'+
     '<li class="formulario"><a href="index.html">Inicio</a></li>'+
